e83fe1aea2a3d61b99aac57b9beeb928be455763
KUNAL KUMAR SAW <55887762+Kunalkumarsaw@users.noreply.github.com>
2022-10-04 15:14:38 +0530

    Add files via upload

---
+++ testing3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = (int)1e5 + 123;
+const int C = 101;
+const int P = 11;
+
+int n, q, c;
+int cnt[P][C][C];
+
+int main()
+{
+    cout << (-4%3) << endl;
+
+    return 0;
+}
